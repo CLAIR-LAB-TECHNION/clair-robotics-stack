708c5fab67cff9aa50f73d5493df489725599e28
yuvalgos <yuvalgos@gmail.com>
2025-02-19 12:53:35 +0200

    started writing tutorials

---
+++ ur_lab/examples/examples_utils.py
@@ -0,0 +1,5 @@
+def run_example(func, name):
+    print("\033[93m ----------------------- \033[0m")
+    print("\033[93m Running Example: ", name, "\033[0m")
+    func()
+    print("\033[93m ----------------------- \033[0m")
