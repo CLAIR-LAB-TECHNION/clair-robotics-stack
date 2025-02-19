cbb05e284db4f49f086a719705b2dc0b933a2946
yuvalgos <yuvalgos@gmail.com>
2025-02-19 12:53:35 +0200

    started writing tutorials

--- ur_lab/examples/2_motion_planner.py
+++ ur_lab/examples/2_motion_planner.py
@@ -1,18 +1,14 @@
-
-
-
-def basic_inteface_example():
-    # moveJ, moveL, movePath, getActualQ, getTCP Pose, refer to ur_urtde
-    pass
-
 def motion_planner_examples():
     # and gt
+    # TODO name
     pass
+
+
 def motion_planning_example():
     # Robot with motion planning and gt
     pass
+    # TODO - logging
 
 def manipulation_inteface():
     # grasping, grasping from different directions, etc..
     pass
-
